c17156f0a1c9193ab3dcc9177f24a747b0fdea63
salsabilasalimah <salsabila1852@gmail.com>
2025-09-10 17:57:36 +0700

    revisi

--- football_news/settings.py
+++ football_news/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "salsabila-salimah-footballnews.pbp.cs.ui.ac.id"]
 CSRF_TRUSTED_ORIGINS = [
-    "<https://salsabila-salimah-footballnews.pbp.cs.ui.ac.id>"
+    "https://salsabila-salimah-footballnews.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
